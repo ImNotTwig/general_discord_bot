1b16e85dcf3a813f2fa28cb0b2775bb96cf28ffc
twig <106347250+ImNotTwig@users.noreply.github.com>
2022-10-11 15:01:49 -0500

    added option to use an env variable or config.json

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import discord
 from discord.ext import commands
 import asyncio
 import json
+import os
 
 from cogs.Unbound.unbound import UnboundCommands
 from cogs.Music.music import MusicCommands
+
+###############################################################################################################
 
 with open("config.json", 'r', encoding="utf8") as file:
     config = json.load(file)
@@ -15,6 +18,8 @@ intents.voice_states = True
 intents.members = True
 
 bot = commands.Bot(command_prefix=config['prefix'], intents=intents, help_command=None)
+
+###############################################################################################################
 
 @bot.event
 async def on_ready():
@@ -27,7 +32,6 @@ async def on_ready():
 async def on_command_error(ctx, error):
     print(f'Error: {error}')
 
-
 @bot.event
 async def on_voice_state_update(member, before, after):
     voice_state = member.guild.voice_client
@@ -36,9 +40,19 @@ async def on_voice_state_update(member, before, after):
 
     if len(voice_state.channel.members) == 1:
         await voice_state.disconnect()
-
+        
+###############################################################################################################
+        
 if __name__ == '__main__':
     asyncio.run(bot.add_cog(UnboundCommands(bot)))
     asyncio.run(bot.add_cog(MusicCommands(bot)))
-    bot.run(config['tokens']["discord_token"])
+    if "tokens" in config.keys():
+        bot.run(config['tokens']["discord_token"])
+    elif os.getenv("discord_token"):
+        bot.run(os.getenv('discord_token'))
+    else:
+        print("""You have not supplied a token either:
 
+add the discord_token field in your .env file
+or
+add the discord_token field in your config.json""")
