c5b03f2c2cffeefd687dd070ddc6edd517d0d985
ImNotTwig <ImNotTwig@gmail.com>
2022-10-11 23:17:43 -0500

    fixed some spacing in the code

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ async def on_ready():
     #setting status to "listening to {prefix}help" eg: "listening to ~help"
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{config["prefix"]}help'))
 
-
 @bot.event
 async def on_command_error(ctx, error):
     print(f'Error: {error}')
@@ -40,16 +39,19 @@ async def on_voice_state_update(member, before, after):
 
     if len(voice_state.channel.members) == 1:
         await voice_state.disconnect()
-        
+
 ###############################################################################################################
-        
+
 if __name__ == '__main__':
     asyncio.run(bot.add_cog(UnboundCommands(bot)))
     asyncio.run(bot.add_cog(MusicCommands(bot)))
+
     if "tokens" in config.keys():
         bot.run(config['tokens']["discord_token"])
+
     elif os.getenv("discord_token"):
         bot.run(os.getenv('discord_token'))
+
     else:
         print("""You have not supplied a token either:
 
