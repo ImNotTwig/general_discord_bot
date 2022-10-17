1ecea27287af447fd4e3d3d14b805c2ed435944b
twig <106347250+ImNotTwig@users.noreply.github.com>
2022-10-17 19:20:07 +0000

    fixed some potential errors in levelsystem.py and did some code formatting

--- cogs/LevelSystem/levelsystem.py
+++ cogs/LevelSystem/levelsystem.py
@@ -1,5 +1,4 @@
 import discord
-from discord import Member
 from discord.ext import commands
 import json
 import asyncio
@@ -21,6 +20,15 @@ for server in levels:
 # xp_needed formula is:
 # 5*(level^2) + (50*level) + 100 - current_xp
 
+def new_member(server, author):
+    levels[server][author] = {}
+    levels[server][author]['level'] = 0
+    levels[server][author]['total_xp'] = 0
+    levels[server][author]['current_xp'] = 0
+    levels[server][author]['xp_needed'] = 100
+    levels[server][author]['can_gain_xp'] = True
+
+
 ############-LEVELSYSTEM COMMANDS-#############################################################################
 
 class LevelSystemCommands(commands.Cog):
@@ -41,42 +49,57 @@ class LevelSystemCommands(commands.Cog):
                 # title
                 embed = discord.Embed(title=f"{ctx.author.name}'s level stats")
                 # Name of the user
-                embed.add_field(name="Name", value=ctx.author.mention, inline=True)
+                embed.add_field(name="Name",
+                                value=ctx.author.mention,
+                                inline=True)
                 # current_xp that the user has until next level up
-                embed.add_field(name="Xp", value=levels[server][author]['current_xp'], inline=True)
+                embed.add_field(name="Xp",
+                                value=levels[server][author]['current_xp'],
+                                inline=True)
                 # current level of the user
-                embed.add_field(name="Level", value=levels[server][author]['level'], inline=True)
-                
+                embed.add_field(name="Level",
+                                value=levels[server][author]['level'],
+                                inline=True)
+
                 current_xp = levels[server][author]['current_xp']
                 xp_needed = levels[server][author]['xp_needed']
                 xp_needed_to_lvl_up = xp_needed - current_xp
-                
-                amount_per_box = xp_needed / 20                
+
+                amount_per_box = xp_needed / 20
                 current_boxes = current_xp / amount_per_box
                 boxes_left = xp_needed_to_lvl_up / amount_per_box
                 # there should be 20 boxes when the embed is sent
                 # a percentage of white and blue squares should correspond to the current_xp and total_xp - current_xp
-                embed.add_field(name="Progress Bar [level]", value=(int(current_boxes)) * ":blue_square:" + (int(boxes_left)) * ":white_large_square:", inline=False)
+                embed.add_field(name="Progress Bar [level]",
+                                value=(int(current_boxes)) * ":blue_square:" +
+                                (int(boxes_left)) * ":white_large_square:",
+                                inline=False)
                 embed.set_thumbnail(url=ctx.author.avatar.url)
-                
+
                 await ctx.send(embed=embed)
-        # if the author is *not* in the levels dict already
+            # if the ~member~ is not in the levels dict already
+            else:
+                # create new user data
+                new_member(server, author)
+                with open('cogs/LevelSystem/levels.json', 'w') as file:
+                    json.dump(levels, file, indent=4)
+
+        # if the ~server~ is not in the levels dict already
         else:
             levels[server] = {}
             # create new user data
-            levels[server][author] = {}
-            levels[server][author]['level'] = 0
-            levels[server][author]['total_xp'] = 0
-            levels[server][author]['current_xp'] = 0
-            levels[server][author]['xp_needed'] = 100
-            levels[server][author]['can_gain_xp'] = True
+            new_member(server, author)
             # sending the embed
-            
+
             embed = discord.Embed(title=f"{ctx.author.name}'s level stats")
             embed.add_field(name="Name", value=ctx.author.mention, inline=True)
-            embed.add_field(name="Xp", value=levels[server][author]['current_xp'], inline=True)
-            embed.add_field(name="Level", value=levels[server][author]['level'], inline=True)
-            
+            embed.add_field(name="Xp",
+                            value=levels[server][author]['current_xp'],
+                            inline=True)
+            embed.add_field(name="Level",
+                            value=levels[server][author]['level'],
+                            inline=True)
+
             current_xp = levels[server][author]['current_xp']
             xp_needed = levels[server][author]['xp_needed']
             xp_needed_to_lvl_up = xp_needed - current_xp
@@ -84,8 +107,11 @@ class LevelSystemCommands(commands.Cog):
             amount_per_box = xp_needed / 20
             current_boxes = current_xp / amount_per_box
             boxes_left = xp_needed_to_lvl_up / amount_per_box
-                            
-            embed.add_field(name="Progress Bar [level]", value=(int(current_boxes)) * ":blue_square:" + (int(boxes_left)) * ":white_large_square:", inline=False)
+
+            embed.add_field(name="Progress Bar [level]",
+                            value=(int(current_boxes)) * ":blue_square:" +
+                            (int(boxes_left)) * ":white_large_square:",
+                            inline=False)
             embed.set_thumbnail(url=ctx.author.avatar.url)
             await ctx.send(embed=embed)
 
@@ -98,42 +124,42 @@ class LevelSystemCommands(commands.Cog):
             server_enabler[str(message.guild.id)] = False
             with open('cogs/LevelSystem/server_level_system_enabler.json', 'r+') as file:
                 json.dump(server_enabler, file, indent=4)
-
+        # if the message starts with the prefix ignore it
         if message.content.startswith(str({config['prefix']})):
             return
-        
-        # if the server has the level system turned on 
+
+        # if author is a bot
+        if message.author.bot is True:
+            return
+
+        # if the server has the level system turned on
         if server_enabler[str(message.guild.id)] is True:
 
             server = str(message.guild.id)
             author = str(message.author.id)
 
-            # if author is a bot
-            if message.author.bot is True:
-                return
-            
             # if the server isnt in the levels dict yet, add it
-            if server not in levels.keys():    
+            if server not in levels.keys():
                 levels[server] = {}
             if author not in levels[server]:
                 # create a new entry in the json with default level 0 values
-                levels[server][author] = {}
-                levels[server][author]['level'] = 0
-                levels[server][author]['total_xp'] = 0
-                levels[server][author]['current_xp'] = 0
-                levels[server][author]['xp_needed'] = 100
-                levels[server][author]['can_gain_xp'] = True
+                new_member(server, author)
 
                 with open('cogs/LevelSystem/levels.json', 'w') as file:
                     json.dump(levels, file, indent=4)
-                          
+
             # if the author can gain xp
             if levels[server][author]['can_gain_xp'] is True:
                 # increase authors total_xp and current_xp
-                levels[server][author]['current_xp'] += random.randint(config['level_system']['xp_per_message'][0], config['level_system']['xp_per_message'][1])
-                levels[server][author]['total_xp'] += random.randint(config['level_system']['xp_per_message'][0], config['level_system']['xp_per_message'][1])
+                levels[server][author]['current_xp'] += random.randint(
+                    config['level_system']['xp_per_message'][0],
+                    config['level_system']['xp_per_message'][1])
                 
-                # if the current_xp is over or equal to the xp_needed 
+                levels[server][author]['total_xp'] += random.randint(
+                    config['level_system']['xp_per_message'][0],
+                    config['level_system']['xp_per_message'][1])
+
+                # if the current_xp is over or equal to the xp_needed
                 if levels[server][author]['current_xp'] >= levels[server][author]['xp_needed']:
                     # calculate how much current_xp went over xp_needed if it did
                     if levels[server][author]['current_xp'] > levels[server][author]['xp_needed']:
@@ -143,17 +169,14 @@ class LevelSystemCommands(commands.Cog):
                     else:
                         levels[server][author]['current_xp'] = 0
 
-                    # increment the authors level by 1    
+                    # increment the authors level by 1
                     levels[server][author]['level'] += 1
-                    #setting the new xp_needed according to the formula defined at the top of this file
+                    # setting the new xp_needed according to the formula defined at the top of this file
                     levels[server][author]['xp_needed'] = 5 * (levels[server][author]['level'] ^ 2) + (50 * levels[server][author]['level']) + 100
-                
+
                 # write the new xp amounts to levels.json
                 with open('cogs/LevelSystem/levels.json', 'w') as file:
                     json.dump(levels, file, indent=4)
-
-                # because the file gets written before the
-                # can_gain_xp state changes it will never be false in the file
 
                 # don't let the author gain xp until the cooldown is over
                 levels[server][author]['can_gain_xp'] = False
@@ -161,18 +184,22 @@ class LevelSystemCommands(commands.Cog):
                 levels[server][author]['can_gain_xp'] = True
         # if the server has the level system turned off
         else:
-            return    
-            
+            return
+
 ############-LEADERBOARD COMMAND-##############################################################################
 
     @commands.command(name="leaderboard")
     async def leaderboard(self, ctx):
         server = str(ctx.message.guild.id)
-        var = dict()
         server_dict = levels[server]
-        # sorting the users in the levels dict by total xp 
-        rankings = {key: value for key, value in sorted(server_dict.items(), key=lambda dict_item: -dict_item[1]['total_xp'])}
-        
+        # sorting the users in the levels dict by total xp
+        rankings = {
+            key: value
+            for key, value in sorted(
+                server_dict.items(),
+                key=lambda dict_item: -dict_item[1]['total_xp'])
+        }
+
         # adding the fields for the first 10 people in the rankings dict
         i = 0
         embed = discord.Embed(title=f"Rankings for {ctx.guild.name}")
@@ -181,32 +208,37 @@ class LevelSystemCommands(commands.Cog):
                 temp = ctx.guild.get_member(int(x))
                 tempxp = levels[server][x]["total_xp"]
                 templevel = levels[server][x]["level"]
-                embed.add_field(name=f"{i}: {temp.name}", value=f"""Level: {templevel}
+                
+                embed.add_field(name=f"{i}: {temp.name}", 
+                                value=f"""Level: {templevel}
 Total Xp: {tempxp}""", inline=False)
+                
                 i += 1
             except Exception:
                 pass
-            #when the amount of users added is 10 break
+            # when the amount of users added is 10 break
             if i == 10:
                 break
         # sending the embed
         await ctx.channel.send(embed=embed)
-        
+
 ############-LEVELSWITCH COMMAND-##############################################################################
-        
+
     @commands.command(name="levelswitch")
     @commands.has_permissions(manage_messages=True)
     async def levelswitch(self, ctx, arg):
         server = str(ctx.guild.id)
+        
         if arg.lower() == "on":
             server_enabler[server] = True
             await ctx.channel.send(f"Switched the level system on for {ctx.guild.name}")
+            
         if arg.lower() == "off":
             server_enabler[server] = False
             await ctx.channel.send(f"Switched the level system off for {ctx.guild.name}")
 
         with open('cogs/LevelSystem/server_level_system_enabler.json', 'w') as file:
-            json.dump(server_enabler, file, indent=4)    
+            json.dump(server_enabler, file, indent=4)
 
 ############-GIVEXP COMMAND-###################################################################################
 
@@ -216,28 +248,29 @@ Total Xp: {tempxp}""", inline=False)
         try:
             amount_of_xp = int(arg)
         except ValueError:
-            await ctx.channel.send("You did not supply a valid integer amount!")
+            await ctx.channel.send("You did not supply a valid integer amount!"
+                                   )
         server = str(ctx.server.id)
         author = str(member.id)
         levels[server][member]['current_xp'] += amount_of_xp
+        
         # while the author's current_xp is greater or equal to xp_needed for level up
         while levels[server][author]['current_xp'] >= levels[server][author]['xp_needed']:
             # calculate how much current_xp went over xp_needed if it did
             if levels[server][author]['current_xp'] > levels[server][author]['xp_needed']:
                 # set the author's current_xp to the difference between it and the xp_needed
                 levels[server][author]['current_xp'] = levels[server][author]['current_xp'] - levels[server][author]['xp_needed']
-            # if the author's current_xp is equal to xp_needed 
+            # if the author's current_xp is equal to xp_needed
             else:
                 levels[server][author]['current_xp'] = 0
-                
-            # increment the authors level by 1    
+
+            # increment the authors level by 1
             levels[server][author]['level'] += 1
-            #setting the new xp_needed according to the formula defined at the top of this file
+            # setting the new xp_needed according to the formula defined at the top of this file
             levels[server][author]['xp_needed'] = 5 * (levels[server][author]['level'] ^ 2) + (50 * levels[server][author]['level']) + 100
 
             # write the new xp amounts to levels.json
-            with open('cogs/LevelSystem/levels.json', 'w') as file:
-                json.dump(levels, file, indent=4)
-                
-            await ctx.channel.send(f"{ctx.author.name} has given {member.name} {arg} xp.")
-            
+        with open('cogs/LevelSystem/levels.json', 'w') as file:
+            json.dump(levels, file, indent=4)
+
+        await ctx.channel.send(f"{ctx.author.name} has given {member.name} {arg} xp.")
